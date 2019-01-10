ad86761352d4987cfddafc4d1d3d1496f6ede4bb
Manhut69 <kenpokaratefan@gmail.com>
2019-01-10 12:58:17 +0100

    Initial app scaffold

---
+++ app/src/main/java/com/example/clintnieuwendijk/tictactoeclicker/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.clintnieuwendijk.tictactoeclicker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void mainClick(View view){
+        Intent intent;
+        switch (view.getId()){
+            case R.id.singlePlayerStartButton:
+                intent = new Intent(MainActivity.this, SinglePlayerActivity.class);
+                break;
+            case R.id.multiplayerStartButton:
+                intent = new Intent(MainActivity.this, MultiplayerActivity.class);
+                break;
+            case R.id.optionsButton:
+                intent = new Intent(MainActivity.this, OptionsActivity.class);
+                break;
+            case R.id.upgradeButton:
+                intent = new Intent(MainActivity.this, UpgradeActivity.class);
+                break;
+        }
+    }
+}
