46d9cddb944067724b4f2671954d7d8da9837fec
Manhut69 <kenpokaratefan@gmail.com>
2019-01-11 13:25:57 +0100

    Update Game class, layouts

---
+++ app/src/main/java/com/example/clintnieuwendijk/tictactoeclicker/TicTacToeGame.java
@@ -0,0 +1,41 @@
+package com.example.clintnieuwendijk.tictactoeclicker;
+
+/**
+ * Design based on Matrix class by Taaqif Peck
+ * https://github.com/Taaqif/TicTacToe-Android
+ */
+public class TicTacToeGame {
+
+    private int[] Matrix;
+    int width;
+    int height;
+
+    public TicTacToeGame(int width, int height) {
+        this.width = width;
+        this.height = height;
+
+        Matrix = new int[width * height];
+
+        for (int i=0; i < height; i++) {
+            for (int j = 0; j < width; j++){
+                Matrix[i*width + j] = 0;
+            }
+        }
+    }
+
+    public int getState(int row, int col){
+        return Matrix[row*width + col];
+    }
+
+    public void setState(int row, int col, int state){
+        Matrix[row*width + col] = state;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
